e83672ea60fd4ec252655f5b02ddaacaf71a3434
fayzullohabdusoliqov-hue <fayzullohabdusoliqov@gmail.com>
2026-06-25 12:47:19 +0500

    login meyble to finlly

--- src/layouts/Login/Login.jsx
+++ src/layouts/Login/Login.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./Login.css"
 import lightLogo from "../../../public/icon/light logo.png"
 import darkLogo from "../../../public/icon/dark logo.png"
@@ -6,36 +8,48 @@ import darkLogo from "../../../public/icon/dark logo.png"
 function Login({mode}) {
   const APIKEY = "AIzaSyBSoQog_dss1CgWGq5lTJ0g5iR1R7Waq_M"
   const [isLoginOrRegistor, setIsLoginOrRegistor] = useState(true)
-  const loginEmail = useRef(null)
-  const loginPassword = useRef(null)
-  const registorEmail = useRef(null)
-  const registerPassword = useRef(null)
-  const registerName = useRef(null)
-  const registerImage = useRef(null)
+  const [loginObject, setLoginObject] = useState({email: "", password: ""})
+  const [registorObject, setRegistorObject] = useState({email: "", password:"",name:"", image: null})
 
-  async function registor(email, password, name, image){
+  async function registor(registorObject){
     try{
+      if(!registorObject.email || !registorObject.password || !registorObject.name || !registorObject.image){
+        toast.error("You have to change input info")
+        return
+      }
+
       const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${APIKEY}`,{
         method: "POST",
         headers:{
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          email: email,
-          password: password
+          email: registorObject.email,
+          password: registorObject.password
         })
       })
       const data = await res.json()
-      localStorage.setItem("token", data?.idToken)
-      localStorage.setItem("localId", data?.localId)
 
-      createAccaunt(email, password, name, image)
+      if(data){
+        localStorage.setItem("token", data?.idToken)
+        localStorage.setItem("localId", data?.localId)
+        toast.success("You are creating accaunt!")
+        createAccaunt(registorObject)
+      }else{
+        toast.error(`Error: your info have the problem`)
+      }
     }catch(err){
       console.log(err.message)
+      toast.error(`error ${err.message}`)
     }
   }
-  async function createAccaunt(email, password, name, image){
+  async function createAccaunt(registorObject){
     try{
+      if(!registorObject.email || !registorObject.password || !registorObject.name || !registorObject.image){
+        toast.error("You have to change input info")
+        return
+      }
+
       const reader = new FileReader()
       const token = localStorage.getItem("token")
       const localId = localStorage.getItem("localId")
@@ -63,23 +77,35 @@ function Login({mode}) {
     }
   }
 
-  async function login(email, password){
+  async function login(loginObject){
     try{
+      if(!loginObject.email || !loginObject.password){
+        toast.error("You have to change input info")
+        return
+      }
+
       const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${APIKEY}`,{
         method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          email: email,
-          password: password
+          email: loginObject.email,
+          password: loginObject.password
         })
       })
       const data = await res.json()
-      localStorage.setItem("token", data?.idToken)
-      localStorage.setItem("localId", data?.localId)
+      
+      if(data){
+        localStorage.setItem("token", data?.idToken)
+        localStorage.setItem("localId", data?.localId)
+        toast.success("Successfully logged in!")
+      }else{
+        toast.error(`Error: your info have the problem`)
+      }
     }catch(err){
       console.log(err.message)
+      toast.error(`error ${err.message}`)
     }
   }
   return (<div className='body'>
@@ -88,49 +114,63 @@ function Login({mode}) {
           <div className="conteyner login__wrapper">
             <form className="login__form" onSubmit={(evt) => {
               evt.preventDefault()
-              login(loginEmail.current.value, loginPassword.current.value)
+              login(loginObject)
             }}>
               <img src={mode? lightLogo : darkLogo} alt="" className="login_logo" />
               <div className="login__content">
                 <label htmlFor="login-email" className="login_label">change your email:</label>
-                <input id='login-email' type="email" className="login_input" ref={loginEmail}/>
+                <input id='login-email' type="email" className="login_input" onChange={
+                  (evt) => setLoginObject({...loginObject, email: evt.target.value})
+                }/>
               </div>
               <div className="login__content">
                 <label htmlFor="login-password" className="login_label">change your password:</label>
-                <input id='login-password' type="password" className="login_input" ref={loginPassword}/>
+                <input id='login-password' type="password" className="login_input" onChange={
+                  (evt) => setLoginObject({...loginObject, password: evt.target.value})
+                }/>
               </div>
               <button className="login_btn">sign in</button>
               <small onClick={() => {setIsLoginOrRegistor(!isLoginOrRegistor)}} className="login_small">If you don't have an account, please register.</small>
             </form>
           </div>
+          <ToastContainer/>
         </section>}
         {!isLoginOrRegistor && <section className="registor">
           <div className="conteyner registor__wraper">
             <form className="login__form" onSubmit={(evt) => {
               evt.preventDefault()
-              registor(registorEmail.current.value, registerPassword.current.value, registerName.current.value, registerImage.current.files[0])
+              registor(registorObject)
             }}>
               <img src={mode? lightLogo : darkLogo} alt="" className="login_logo" />
               <div className="login__content">
                 <label htmlFor="email" className="login_label">change your email:</label>
-                <input id='email' type="email" className="login_input" ref={registorEmail}/>
+                <input id='email' type="email" className="login_input" onChange={
+                  (evt) => setRegistorObject({...registorObject, email: evt.target.value})
+                }/>
               </div>
               <div className="login__content">
                 <label htmlFor="password" className="login_label">change your password:</label>
-                <input id='password' type="password" className="login_input" ref={registerPassword}/>
+                <input id='password' type="password" className="login_input" onChange={
+                  (evt) => setRegistorObject({...registorObject, password: evt.target.value})
+                }/>
               </div>
               <div className="login__content">
                 <label htmlFor="name" className="login_label">change your name:</label>
-                <input id='name' type="name" className="login_input" ref={registerName}/>
+                <input id='name' type="name" className="login_input" onChange={
+                  (evt) => setRegistorObject({...registorObject, name: evt.target.value})
+                }/>
               </div>
               <div className="login__content">
                 <label htmlFor="file" className="login_label">change your image:</label>
-                <input id='file' type="file" className="login_input" ref={registerImage}/>
+                <input id='file' type="file" className="login_input" onChange={
+                  (evt) => setRegistorObject({...registorObject, image: evt.target.files[0]})
+                }/>
               </div>  
               <button className="login_btn">sign up</button>
               <small onClick={() => {setIsLoginOrRegistor(!isLoginOrRegistor)}} className="login_small">If you have an account, log in.</small>
             </form>
           </div>
+          <ToastContainer/>
         </section>}
       </main>
   </div>)
